88ad034c9d5a3c1e08670ac29ed40548fb2aeb04
Nirav Patel <nirav.ce.2008@gmail.com>
2019-06-28 17:37:20 +0530

    public files

--- public/js/app.js
+++ public/js/app.js
@@ -1494,7 +1494,7 @@ module.exports = function spread(callback) {
 
 
 var bind = __webpack_require__(/*! ./helpers/bind */ "./node_modules/axios/lib/helpers/bind.js");
-var isBuffer = __webpack_require__(/*! is-buffer */ "./node_modules/axios/node_modules/is-buffer/index.js");
+var isBuffer = __webpack_require__(/*! is-buffer */ "./node_modules/is-buffer/index.js");
 
 /*global toString:true*/
 
@@ -1829,10 +1829,10 @@ module.exports = {
 
 /***/ }),
 
-/***/ "./node_modules/axios/node_modules/is-buffer/index.js":
-/*!************************************************************!*\
-  !*** ./node_modules/axios/node_modules/is-buffer/index.js ***!
-  \************************************************************/
+/***/ "./node_modules/is-buffer/index.js":
+/*!*****************************************!*\
+  !*** ./node_modules/is-buffer/index.js ***!
+  \*****************************************/
 /*! no static exports found */
 /***/ (function(module, exports) {
 
@@ -2059,6 +2059,11 @@ process.umask = function() { return 0; };
  * building robust, powerful web applications using Vue and Laravel.
  */
 window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js");
+window.axios.defaults.headers.common = {
+  'X-Requested-With': 'XMLHttpRequest',
+  'X-CSRF-TOKEN': document.querySelector('meta[name="csrf-token"]').getAttribute('content')
+}; //require('jquery-serializejson');
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -2075,16 +2080,147 @@ window.axios = __webpack_require__(/*! axios */ "./node_modules/axios/index.js")
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
+window.extractColumn = function (arr, column) {
+  return arr.map(function (x) {
+    return x[column];
+  });
+};
+
+window.dtable = function (identity, options) {
+  var tableOptions = {
+    dom: 'Bfrtip',
+    buttons: {
+      buttons: [{
+        extend: 'copy',
+        text: 'Copy',
+        title: $('h1').text(),
+        exportOptions: {
+          columns: ':not(.no-print)'
+        },
+        footer: true
+      }, {
+        extend: 'excel',
+        text: 'Excel',
+        title: $('h1').text(),
+        exportOptions: {
+          columns: ':not(.no-print)'
+        },
+        footer: true
+      }, {
+        extend: 'csv',
+        text: 'Csv',
+        title: $('h1').text(),
+        exportOptions: {
+          columns: ':not(.no-print)'
+        },
+        footer: true
+      }, {
+        extend: 'pdf',
+        text: 'Pdf',
+        title: $('h1').text(),
+        exportOptions: {
+          columns: ':not(.no-print)'
+        },
+        footer: true
+      }, {
+        extend: 'print',
+        text: 'Print',
+        title: $('h1').text(),
+        exportOptions: {
+          columns: ':not(.no-print)'
+        },
+        footer: true,
+        autoPrint: true
+      }],
+      dom: {
+        container: {
+          className: 'dt-buttons'
+        },
+        button: {
+          className: 'btn btn-primary'
+        }
+      }
+    }
+  };
+  return $(identity).DataTable($.extend({}, tableOptions, options));
+};
+
+var submitDeleteResourceForm = function submitDeleteResourceForm(deleteUrl) {
+  $('<form>', {
+    'method': 'POST',
+    'action': deleteUrl,
+    'target': '_top'
+  }).append($('<input>', {
+    'name': '_token',
+    'value': $('meta[name="csrf-token"]').attr('content'),
+    'type': 'hidden'
+  })).append($('<input>', {
+    'name': '_method',
+    'value': 'DELETE',
+    'type': 'hidden'
+  })).hide().appendTo("body").submit();
+};
+
+window.buildEditAction = function (link) {
+  return $("<div />").append($('<a />', {
+    html: '<i class="la la-pencil"></i>',
+    href: link || 'javascript:void(0)',
+    title: 'Edit',
+    "class": 'btn btn-md btn-round-sm btn-info ml-1 mr-1'
+  })).html();
+};
+
+window.buildDeleteAction = function (link) {
+  return $("<div />").append($('<a />', {
+    html: '<i class="la la-trash"></i>',
+    href: link || 'javascript:void(0)',
+    title: 'Delete',
+    "class": 'btn btn-md btn-round-sm btn-danger ml-1 mr-1 delete-confirmation-button'
+  })).html();
+};
+
+window.buildViewAction = function (link) {
+  return $("<div />").append($('<a />', {
+    html: '<i class="la la-eye"></i>',
+    href: link || 'javascript:void(0)',
+    title: 'View',
+    "class": 'btn btn-md btn-round-sm btn-info ml-1 mr-1'
+  })).html();
+};
+
+window.initConfirmationOnDelete = function () {
+  $('body').on('click', '.delete-confirmation-button', function (event) {
+    event.preventDefault();
+    var deleteUrl = $(this).attr('href');
+
+    if (confirm('Are you sure you want to delete ? ')) {
+      submitDeleteResourceForm(deleteUrl);
+    }
+  });
+};
+
+/***/ }),
+
+/***/ "./resources/sass/app.scss":
+/*!*********************************!*\
+  !*** ./resources/sass/app.scss ***!
+  \*********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
 /***/ }),
 
 /***/ 0:
-/*!***********************************!*\
-  !*** multi ./resources/js/app.js ***!
-  \***********************************/
+/*!*************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
+  \*************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /var/www/html/invoice/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! C:\laragon\www\invoice-backend\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\invoice-backend\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
