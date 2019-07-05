c71d8c3253b657d19c34fa9ae910fa61d6d09abb
Nirav Patel <nirav.ce.2008@gmail.com>
2019-07-05 16:41:53 +0530

    Frontend resources

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,6 +14,7 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css');
 mix.copyDirectory('resources/assets', 'public');
+mix.copyDirectory('resources/frontend', 'public');
 
 mix.js('resources/js/pages/invoice/add.js','public/js/pages/invoice-add.js');
 mix.js('resources/js/pages/invoice/index.js','public/js/pages/invoice-list.js');
